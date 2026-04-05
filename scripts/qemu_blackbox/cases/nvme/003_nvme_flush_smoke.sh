bb2d3f7aa2cd97bf7197b72402d8264e30cc9e98
zifengyang <zifengyang@zifengyangs-Mac-Studio.local>
2026-04-05 08:44:55 -0400

    Harden blackbox PR smoke coverage

--- scripts/qemu_blackbox/cases/nvme/003_nvme_flush_smoke.sh
+++ scripts/qemu_blackbox/cases/nvme/003_nvme_flush_smoke.sh
@@ -12,4 +12,8 @@ hfsss_case_guest_run \
     "dd if=/dev/zero of=$HFSSS_GUEST_NVME_DEV bs=4096 count=8 oflag=direct conv=fsync"
 hfsss_case_run_nvme "nvme-flush" "flush $HFSSS_GUEST_NVME_CTRL --namespace-id=1"
 
-hfsss_case_assert_file_contains "$HFSSS_CASE_ARTIFACT_DIR/prep-write.stdout" "records out"
+hfsss_case_assert_any_file_contains \
+    "records out" \
+    "$HFSSS_CASE_ARTIFACT_DIR/prep-write.stdout" \
+    "$HFSSS_CASE_ARTIFACT_DIR/prep-write.stderr"
+hfsss_case_assert_file_contains "$HFSSS_CASE_ARTIFACT_DIR/nvme-flush.stdout" "Flush: success|NVMe Flush: success"
